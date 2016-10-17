b0a8ce3f740a1b2d6596408a76dc5dff4e4b5890
katyou <mainichiaisatsu@outlook.com>
2016-10-17 16:36:53 +0900

    initial commit

---
+++ calculate.py
@@ -0,0 +1,150 @@
+# coding:UTF-8
+
+def textwrite(vol, cur):
+	from matplotlib import pyplot as plt
+	import numpy as np
+	import os
+
+	#calculate arduino
+	for line in range (1,300):
+		vol = (vol*55)/1023
+		cur = (cur*5)/(1023*11)
+
+def content(volta, currenting, powering ):
+	import numpy as np
+	from math import floor
+	from math import ceil
+	import time
+
+	a = np.array([volta, currenting, powering])
+	support = np.round(a,3)
+	data = np.transpose(support)
+
+	sample = open("test.txt", "w")
+	sample.write(str(data))
+	sample.close()
+	x = 0
+
+	for i in range (1, 300):
+		index = data[x, 2]
+
+		if index == 2.708:                   #取り出したい電力の値
+			voltagein = data[x, 0]
+			duty = 2.9/(2.9 + voltagein)
+			sendingvalue = duty * 1023
+			print ("No.1")
+			print (str(duty))
+			time.sleep(5)
+			break
+
+		else:
+			x += 1
+
+	if not index == 2.708:
+		nextvalue = np.round(a,2)
+		datatwo = np.transpose(nextvalue)
+		y = 0
+		sample = open("test1.txt", "w")
+		sample.write(str(datatwo))
+		sample.close()
+
+		for i in range (1, 300):
+			index = datatwo[y, 2]
+
+			if index == 2.71:               #取り出したい電力の値 part 2
+				voltagein = datatwo[y, 0]
+				duty = 2.9/(2.9 + voltagein)
+				sendingvalue = duty * 1023
+				print ("NO.2")
+				print (str(duty))
+				time.sleep(5)
+				break
+			else:
+				y += 1
+
+	if index != 2.708 and index != 2.71:
+		nextvalue = np.round(a,1)
+		datathree = np.transpose(nextvalue)
+		z = 0
+		sample = open("test2.txt", "w")
+		sample.write(str(datathree))
+		sample.close()
+
+
+		for i in range (1, 300):
+			index = datathree[z, 2]
+
+			if index == 2.7:                #取り出したい電力の値 part 3
+				voltagein = datathree[z,0]
+				duty = 2.9/(2.9 + voltagein)
+				sendingvalue = duty * 1023
+				print ("No.3")
+				print (str(duty))
+				time.sleep(5)
+				break
+			else:
+				z += 1
+
+	if index != 2.771 and index != 2.77:
+		if index != 2.8:
+			nextvalue = np.round(a,2)
+			datafinish = np.transpose(nextvalue)
+			k = 0
+			sample = 0
+
+			for i in range (1, 300):
+				index = datafinish[k, 2]
+				if sample < index and index <= 2.35:
+					sample = index
+					voltagein = datafinish[k, 0]
+				else:
+					k += 1
+
+			duty = 2.9/(2.9 + voltagein)
+			sendingvalue = duty * 1023
+
+
+
+		print ("No.4")
+		print (str(duty))
+		time.sleep(3)
+
+
+	fusin = int(sendingvalue)
+	dutying = str(fusin) + '\r\n'
+	print (str(dutying))
+
+	import serial
+	import os
+	import shutil
+	ser = serial.Serial("/dev/ttyACM2", 115200)
+	time.sleep(2)
+
+	ser.write(bytes([fusin]))
+
+
+# gragh 表示,保存するためのメソッド
+def graphmodel():
+  from matplotlib import pyplot as plt
+  import numpy as np
+  import time
+  import os
+  import datetime
+
+  fig = plt.figure()
+
+  plt.xlim([5.5,23])
+  plt.ylim([0,0.3])
+  plt.plot(voltage, current, '-o')
+
+  s.chdir("/home/ienaga/デスクトップ/python_class_sample/gragh") #change directly
+
+  plt.savefig('sample.png')
+
+  #text file calculate finish
+
+  dailytime = datetime.datetime.now()
+  newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(dailytime)
+  os.rename("sample.png",newname)
+
+  os.remove('sample.txt')
---
+++ solar.py
@@ -0,0 +1,85 @@
+# coding:UTF-8
+
+while True:
+	import os
+	import datetime
+	import shutil
+
+	os.mkdir("sample")
+	os.mkdir("gragh")
+
+	oclock = datetime.datetime.today()
+
+	while 6 <= oclock.hour <= 20:
+		import serial
+		import os
+		import shutil
+		import time
+
+		os.chdir("/home/ienaga/デスクトップ/python_class_sample/sample") #change directly
+		ser = serial.Serial("/dev/ttyACM0",9600)
+		time.sleep(2)
+		ser.write(b'z')
+		serial = open("sample.txt", 'w')
+		print("begin to measure")
+
+		for i in range (1,300):
+			val = ser.readline()
+			print(val.decode('utf-8'))
+			serial.write(val.decode('utf-8'))
+
+		ser.write(b"y")
+		serial.close()
+
+		from matplotlib import pyplot as plt
+		import numpy as np
+		import os
+
+		fig = plt.figure()
+		xm, ym = np.loadtxt('sample.txt', delimiter = ',', unpack = True)
+
+		for line in range (1,300):
+			vol = (xm*55)/1023
+			cur = (ym*5)/(1023*11)
+
+		sampledata = (vol, cur)
+
+		os.chdir("/home/ienaga/デスクトップ/python_class_sample/gragh") #change directly
+
+		fig = plt.figure()
+		plt.xlim([5.5,23])
+		plt.ylim([0,0.4])
+		plt.plot(vol, cur, '-o')
+
+		plt.savefig('sample.png')
+
+		#text file calculate finish
+
+		dailytime = datetime.datetime.now()
+		newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(dailytime)
+		os.rename("sample.png",newname)
+
+		os.chdir("/home/ienaga/デスクトップ/python_class_sample/sample") #change directly
+
+		for power in sampledata:
+			power = vol * cur
+
+		sampledata = np.array([vol, cur, power])
+
+		from calculate import content
+		content(vol, cur, power)
+
+		time.sleep(30.0)
+
+		ser.close()
+
+	while oclock.hour > 23:
+		os.chdir("/home/pi/Desktop/python_class_sample") #change directly
+		newdate = datetime.datetime.now()
+		newname = "{0:%Y-%m-%d}".format(newdate)
+		os.rename("sample",newname)
+		
+		time.sleep(60)
+
+	while oclock.hour < 5:
+		time.sleep(120)
