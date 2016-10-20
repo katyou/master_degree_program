453dacad9e061df8ee6af4de76f080b535677196
Masahiro Ienaga <mainichiaisatu@me.com>
2016-10-20 18:01:43 +0900

    Add button action

--- graghic.py
+++ graghic.py
@@ -1,7 +1,10 @@
+# coding:UTF-8
+
 import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4 import QtGui
+import PyQt4.QtCore as QtCore
 import datetime
 
 class MainWindow(QMainWindow):
@@ -22,15 +25,30 @@ class MainWindow(QMainWindow):
         self.textbox.resize(250,30)
         timer = QTimer(self)
         timer.timeout.connect(self.time_draw)
-        timer.start(1000)
+        timer.start(10)
 
         #label追加
-        self.label = QLabel('Duty値', self)
-        self.label.setGeometry(600,500,100,100)
-        self.label.resize(200,200)
+        self.label1 = QLabel('Duty値', self)
+        self.label1.setGeometry(600,500,100,100)
+        self.label1.resize(200,200)
 
-        self.label = QLabel('ここにduty値が来るようにする', self)
-        self.label.setGeometry(600,520,200,100)
+        self.label2 = QLabel('ここにduty値が来るようにする', self)
+        self.label2.setGeometry(600,520,200,200)
+        #self.label2.setPointSize(32)
+
+        button = QtGui.QPushButton('測定スタート',self)
+        button.setGeometry(550,200,200,100)
+        #self.connect(button, QtCore.SIGNAL('clicked()'), self.filename)
+
+        button = QtGui.QPushButton('FINISH',self)
+        button.setGeometry(550,300,200,50)
+        #self.connect(button, QtCore.SIGNAL('clicked()'), self.filename)
+
+        comment = "測定装置停止中"
+        self.label = QLabel(comment, self)
+        self.label.setGeometry(600,10,150,50)
+        #self.label.resize(200,200)
+
         self.show()
 
     def time_draw(self):
@@ -39,6 +57,9 @@ class MainWindow(QMainWindow):
         daystring = d.strftime("%m月%d日 %H時%M分")
         self.statusBar().showMessage(daystring)
         self.textbox.setText(daystr)
+
+    def comment(answer):
+        print("how")
 
 
 
@@ -66,6 +87,11 @@ class MainWindow(QMainWindow):
     # テスト用グラフを生成する
     def createTestGraph(self, dataListSet):
         print('createTestGraph')
+
+# [6.4]
+    #スタートボタン押下時
+    def clickedstart(self, filename):
+        print("start")
 
 
 if __name__ == '__main__':
